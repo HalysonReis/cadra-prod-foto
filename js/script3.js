0fbe13e4a350a3f5d7cd81c6945c29c73365bdbe
Halyson Reis <halysondasilvadosreis@gmail.com>
2025-04-07 16:58:01 -0400

    error submit

--- js/script3.js
+++ js/script3.js
@@ -11,8 +11,63 @@ var btn_cadastrar = document.getElementById('cadastrar')
 
 btn_cadastrar.addEventListener('click', function(e){
     e.preventDefault()
-    alert('Clicou no cadstrar')
+
+    let nome = formulario.nome.value;
+    let cpf = formulario.cpf.value;
+    let email = formulario.email.value;
+    let fone = formulario.fone.value;
+    let cidade = formulario.cidade.value;
+
+    if (nome = ""){
+        alert('preencha o nome');
+    }
+    else if (cpf.length < 11){
+        alert('cpf incompleto');
+    }
+    else if(email = ""){
+        alert('preencha o email');
+    }
+    else if(fone.length < 11){
+        alert('preencha o fone');
+    }
+    else if(cidade = ""){
+        alert('preencha o cidade');
+    }
+
+    e.submitEvent()
+
 })
 
-var formulario = document.getElementById('formulario')
+// usado no mercado
 
+// const formulario = document.getElementById('formulario')
+
+// formulario.addEventListener('submit', function(e){
+//     e.preventDefault()
+
+//     let nome = formulario.nome.value;
+//     let cpf = formulario.cpf.value;
+//     let email = formulario.email.value;
+//     let fone = formulario.fone.value;
+//     let cidade = formulario.cidade.value;
+
+//     if (nome = ""){
+//         alert('preencha o nome');
+//     }
+//     else if (cpf.length < 11){
+//         alert('cpf incompleto');
+//     }
+//     else if(email = ""){
+//         alert('preencha o email');
+//     }
+//     else if(fone.length < 11){
+//         alert('preencha o fone');
+//     }
+//     else if(cidade = ""){
+//         alert('preencha o cidade');
+//     }else {
+//         return true;
+//     }
+
+
+// })
